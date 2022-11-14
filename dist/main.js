443b281cf6ac1a484528ef500d24ffdda66b5aae
Sanjay Jaiswal <55450200+sanjay-xdr@users.noreply.github.com>
2022-11-14 22:21:37 +0530

    setting up webpack

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";document.getElementById("root").innerHTML=`\n<h1> learning web tooling today </h1>\n${(({name:n})=>`\n    <h2> Welcome, ${n} </h2>\n    `)({name:"Sanjay"})}\n`})();
