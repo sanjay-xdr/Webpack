2d4281e1ae3b2f1fa4908f2ac840281839828736
Sanjay Jaiswal <55450200+sanjay-xdr@users.noreply.github.com>
2022-11-19 08:53:46 +0530

    adding css using webpack

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import App from "./App";
 
 const root = document.getElementById("root");
 root.innerHTML = `
-<h1> learning web tooling !!  </h1>
+<h1> learning web tooling today </h1>
 ${App({ name: "Sanjay" })}
 `;
